b1a710d848e54e73dbdc7f639ce189036547f8f7
Favics-T <taiwokolapo533@gmail.com>
2026-04-15 13:42:21 +0100

    added featured product componet

--- src/component/product/FeaturedProduct.jsx
+++ src/component/product/FeaturedProduct.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+const FeaturedProduct = ({ product }) => {
+  return (
+    <div className="bg-gray-100 rounded-3xl p-6 flex flex-col justify-between">
+      <span className="text-xs text-gray-400">Featured</span>
 
-const FeaturedProduct=()=>{
-    return(
-        <div>
+      <img
+        src={product.image}
+        alt={product.name}
+        className="w-full object-contain"
+      />
 
-        </div>
-    )
-}
+      <div className="mt-4">
+        <h3 className="font-medium">{product.name}</h3>
+        <p className="text-sm text-gray-500">{product.description}</p>
+        <p className="mt-2 font-semibold">${product.price}</p>
+      </div>
+    </div>
+  );
+};
 
-export default FeaturedProduct
+export default FeaturedProduct;
